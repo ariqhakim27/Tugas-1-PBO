26593e7e2594761cc9b938ce046b62a7735a35b3
Ariq Hakim <91816220+ariqhakim27@users.noreply.github.com>
2022-11-07 09:09:47 +0700

    Update VirtualDemo.java

--- Tugas1 PBO Ariq Abdurrahman Hakim 2111522006/Tugas1 Ariq Abdurrahman Hakim 2111522006/src/VirtualDemo.java
+++ Tugas1 PBO Ariq Abdurrahman Hakim 2111522006/Tugas1 Ariq Abdurrahman Hakim 2111522006/src/VirtualDemo.java
@@ -1,8 +1,8 @@
 // Ariq Abdurrahman Hakim 2111522006
 public class VirtualDemo {
     public static void main(String[] args) {
-        Gaji s = new Gaji("Wahyu ", "KUBAR ", 3, 5000.00);
-        Pegawai e = new Gaji("Ini Nama ", "Samarinda ", 2, 2500.00);
+        Gaji s = new Gaji("Wahyu ", "KUBAR ", 3, 5000.00); //Polymorphism
+        Pegawai e = new Gaji("Ini Nama ", "Samarinda ", 2, 2500.00); //Polymorphism
         System.out.println("Memanggil mailCheck Berdasarkan Referensi Gaji --");
 
         s.mailCheck();
